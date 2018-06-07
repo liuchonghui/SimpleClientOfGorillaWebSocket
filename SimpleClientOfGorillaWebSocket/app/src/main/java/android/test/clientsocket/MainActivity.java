ec991a8390195cfbb0199fc8f0a25427fb77cd7f
liuchonghui <liuchonghui@xiaomi.com>
2018-06-07 16:25:51 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- SimpleClientOfGorillaWebSocket/app/src/main/java/android/test/clientsocket/MainActivity.java
+++ SimpleClientOfGorillaWebSocket/app/src/main/java/android/test/clientsocket/MainActivity.java
@@ -1,16 +1,12 @@
 package android.test.clientsocket;
 
 import android.annotation.TargetApi;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onClick(View v) {
-                try {
-                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData cd = ClipData.newPlainText(null, hint1.getText().toString());
-                    cm.setPrimaryClip(cd);
-                    Toast.makeText(v.getContext(), "已复制到剪贴板", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
             }
         });
 
@@ -42,14 +31,7 @@ public class MainActivity extends AppCompatActivity {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onClick(View v) {
-                try {
-                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData cd = ClipData.newPlainText(null, hint2.getText().toString());
-                    cm.setPrimaryClip(cd);
-                    Toast.makeText(v.getContext(), "已复制到剪贴板", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
             }
         });
 
@@ -59,14 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onClick(View v) {
-                try {
-                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData cd = ClipData.newPlainText(null, hint3.getText().toString());
-                    cm.setPrimaryClip(cd);
-                    Toast.makeText(v.getContext(), "已复制到剪贴板", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
             }
         });
     }
